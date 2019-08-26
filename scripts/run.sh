131ddf52c92bf79c3b042aaa5daca5e94185c11e
Przemysław Kuczyński <przxmek@gmail.com>
2019-08-26 15:44:43 +0200

    Script to run frontend & backend

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,5 +8,10 @@ docker tag 537194362290.dkr.ecr.us-west-2.amazonaws.com/admin-dashboard-backend:
 docker pull 537194362290.dkr.ecr.us-west-2.amazonaws.com/admin-dashboard-frontend:latest
 docker tag 537194362290.dkr.ecr.us-west-2.amazonaws.com/admin-dashboard-frontend:latest admin-dashboard-frontend:latest
 
-docker run -d -e ENV=$ENV -e POINT_RDS_PASSWORD=$POINT_RDS_PASSWORD admin-dashboard-backend:latest
-docker run -d -p 5000:80 admin-dashboard-frontend:latest
+docker stop admin-dashboard-backend
+docker rm admin-dashboard-backend
+docker run -d --name admin-dashboard-backend -p 5000:5000 -e ENV=$ENV -e POINT_RDS_PASSWORD=$POINT_RDS_PASSWORD admin-dashboard-backend:latest
+
+docker stop admin-dashboard-frontend
+docker rm admin-dashboard-frontend
+docker run -d --name admin-dashboard-frontend -p 3000:80 admin-dashboard-frontend:latest
