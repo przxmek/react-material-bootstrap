af34a7c1ea211a1fa2870ff451c457d59ac1c6d6
Przemysław Kuczyński <przxmek@gmail.com>
2019-08-28 15:33:52 +0200

    Fancy run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-$(aws ecr get-login --no-include-email --region us-west-2)
+$(aws ecr get-login --no-include-email --region us-west-2) || exit 1
 
 docker pull 537194362290.dkr.ecr.us-west-2.amazonaws.com/admin-dashboard-backend:latest
 docker tag 537194362290.dkr.ecr.us-west-2.amazonaws.com/admin-dashboard-backend:latest admin-dashboard-backend:latest
@@ -8,10 +8,20 @@ docker tag 537194362290.dkr.ecr.us-west-2.amazonaws.com/admin-dashboard-backend:
 docker pull 537194362290.dkr.ecr.us-west-2.amazonaws.com/admin-dashboard-frontend:latest
 docker tag 537194362290.dkr.ecr.us-west-2.amazonaws.com/admin-dashboard-frontend:latest admin-dashboard-frontend:latest
 
+echo "Starting admin-dashboard-backend docker.."
 docker stop admin-dashboard-backend
 docker rm admin-dashboard-backend
 docker run -d --name admin-dashboard-backend -p 5000:5000 -e ENV=$ENV -e POINT_RDS_PASSWORD=$POINT_RDS_PASSWORD admin-dashboard-backend:latest
 
+echo "Starting admin-dashboard-frontend docker.."
 docker stop admin-dashboard-frontend
 docker rm admin-dashboard-frontend
 docker run -d --name admin-dashboard-frontend -p 3000:80 admin-dashboard-frontend:latest
+
+echo "Ready!"
+
+echo ""
+echo "You can now view point-admin-dashboard in the browser."
+echo ""
+echo "    Local: http://localhost:3000/"
+echo ""
