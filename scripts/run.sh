6b59643ff5c41efb4de7aba1264c7e746eb6ebbb
Przemysław Kuczyński <przxmek@gmail.com>
2019-09-02 14:53:31 +0200

    Improve run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+if [ -z "$ENV" ]
+then
+      echo "ENV environment variable must be set! Allowed values: 'dev', 'prod'" && exit 1
+fi
+
+
+if [ -z "$POINT_RDS_PASSWORD" ]
+then
+      echo "POINT_RDS_PASSWORD environment variable must be set!" && exit 1
+fi
+
 $(aws ecr get-login --no-include-email --region us-west-2) || exit 1
 
 docker pull 537194362290.dkr.ecr.us-west-2.amazonaws.com/admin-dashboard-backend:latest
@@ -8,20 +19,19 @@ docker tag 537194362290.dkr.ecr.us-west-2.amazonaws.com/admin-dashboard-backend:
 docker pull 537194362290.dkr.ecr.us-west-2.amazonaws.com/admin-dashboard-frontend:latest
 docker tag 537194362290.dkr.ecr.us-west-2.amazonaws.com/admin-dashboard-frontend:latest admin-dashboard-frontend:latest
 
-echo "Starting admin-dashboard-backend docker.."
-docker stop admin-dashboard-backend
-docker rm admin-dashboard-backend
+echo "> Starting admin-dashboard-backend docker.."
+docker rm -f admin-dashboard-backend || true
 docker run -d --name admin-dashboard-backend -p 5000:5000 -e ENV=$ENV -e POINT_RDS_PASSWORD=$POINT_RDS_PASSWORD admin-dashboard-backend:latest
 
-echo "Starting admin-dashboard-frontend docker.."
-docker stop admin-dashboard-frontend
-docker rm admin-dashboard-frontend
+echo "> Starting admin-dashboard-frontend docker.."
+docker rm -f admin-dashboard-frontend || true
 docker run -d --name admin-dashboard-frontend -p 3000:80 admin-dashboard-frontend:latest
 
-echo "Ready!"
+echo "> Ready!"
 
 echo ""
 echo "You can now view point-admin-dashboard in the browser."
 echo ""
 echo "    Local: http://localhost:3000/"
+echo "    API:   http://localhost:5000/"
 echo ""
