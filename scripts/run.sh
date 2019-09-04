61685fa709824c5141eb576fff7c674db0f73783
Przemysław Kuczyński <przxmek@gmail.com>
2019-09-04 18:52:04 +0200

    run.sh should use master tag in docker

--- scripts/run.sh
+++ scripts/run.sh
@@ -13,19 +13,19 @@ fi
 
 $(aws ecr get-login --no-include-email --region us-west-2) || exit 1
 
-docker pull 537194362290.dkr.ecr.us-west-2.amazonaws.com/admin-dashboard-backend:latest
-docker tag 537194362290.dkr.ecr.us-west-2.amazonaws.com/admin-dashboard-backend:latest admin-dashboard-backend:latest
+docker pull 537194362290.dkr.ecr.us-west-2.amazonaws.com/admin-dashboard-backend:master
+docker tag 537194362290.dkr.ecr.us-west-2.amazonaws.com/admin-dashboard-backend:master admin-dashboard-backend:master
 
-docker pull 537194362290.dkr.ecr.us-west-2.amazonaws.com/admin-dashboard-frontend:latest
-docker tag 537194362290.dkr.ecr.us-west-2.amazonaws.com/admin-dashboard-frontend:latest admin-dashboard-frontend:latest
+docker pull 537194362290.dkr.ecr.us-west-2.amazonaws.com/admin-dashboard-frontend:master
+docker tag 537194362290.dkr.ecr.us-west-2.amazonaws.com/admin-dashboard-frontend:master admin-dashboard-frontend:master
 
 echo "> Starting admin-dashboard-backend docker.."
 docker rm -f admin-dashboard-backend || true
-docker run -d --name admin-dashboard-backend -p 5000:5000 -e ENV=$ENV -e POINT_RDS_PASSWORD=$POINT_RDS_PASSWORD admin-dashboard-backend:latest
+docker run -d --name admin-dashboard-backend -p 5000:5000 -e ENV=$ENV -e POINT_RDS_PASSWORD=$POINT_RDS_PASSWORD admin-dashboard-backend:master
 
 echo "> Starting admin-dashboard-frontend docker.."
 docker rm -f admin-dashboard-frontend || true
-docker run -d --name admin-dashboard-frontend -p 3000:80 admin-dashboard-frontend:latest
+docker run -d --name admin-dashboard-frontend -p 3000:80 admin-dashboard-frontend:master
 
 echo "> Ready!"
 
