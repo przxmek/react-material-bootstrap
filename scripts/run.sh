8234281fd0de41dcf097f5298bf4a45cbffd3ad9
Przemysław Kuczyński <przxmek@gmail.com>
2019-09-05 14:05:34 +0200

    Add mailjet env variables
    - MJ_APIKEY_PUBLIC
    - MJ_APIKEY_PRIVATE

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,10 +5,19 @@ then
       echo "ENV environment variable must be set! Allowed values: 'dev', 'prod'" && exit 1
 fi
 
-
 if [ -z "$POINT_RDS_PASSWORD" ]
 then
       echo "POINT_RDS_PASSWORD environment variable must be set!" && exit 1
+fi
+
+if [ -z "$MJ_APIKEY_PUBLIC" ]
+then
+      echo "MJ_APIKEY_PUBLIC environment variable must be set!" && exit 1
+fi
+
+if [ -z "$MJ_APIKEY_PRIVATE" ]
+then
+      echo "MJ_APIKEY_PRIVATE environment variable must be set!" && exit 1
 fi
 
 $(aws ecr get-login --no-include-email --region us-west-2) || exit 1
@@ -21,7 +30,7 @@ docker tag 537194362290.dkr.ecr.us-west-2.amazonaws.com/admin-dashboard-frontend
 
 echo "> Starting admin-dashboard-backend docker.."
 docker rm -f admin-dashboard-backend || true
-docker run -d --name admin-dashboard-backend -p 5000:5000 -e ENV=$ENV -e POINT_RDS_PASSWORD=$POINT_RDS_PASSWORD admin-dashboard-backend:master
+docker run -d --name admin-dashboard-backend -p 5000:5000 -e ENV=$ENV -e POINT_RDS_PASSWORD=$POINT_RDS_PASSWORD -e MJ_APIKEY_PUBLIC=$MJ_APIKEY_PUBLIC -e MJ_APIKEY_PRIVATE=$MJ_APIKEY_PRIVATE admin-dashboard-backend:master
 
 echo "> Starting admin-dashboard-frontend docker.."
 docker rm -f admin-dashboard-frontend || true
